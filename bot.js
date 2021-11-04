0132b55e2c0b0576635dbea52a056b14a33f1d62
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-03 21:19:27 -0400

    bot.js
    A bot that plays Cookie Clicker for you.

---
+++ bot.js
@@ -0,0 +1,12 @@
+// In developement
+console.log("Copyright (c) 2021 eunhyobenjaminkim \n" +
+"Permission is hearby granted, free of charge. You may copy, modify, merge, publish, distribute, sublicense, and/or sell \n" +
+"copies of the Programs as long as you are subject to the following conditions: \n" +
+"You are to give credit and/or provide the link to the original Program. Note, that I will not be responsible for any unwanted changes to your game. \n" +
+"Feel free to modify the program to your desire, following the conditions above. \n" +
+"Thank you, and Enjoy!")
+document.getElementById("bakeryName").click();
+document.getElementById("bakeryNameInput").value = "Bot";
+document.getElementById("promptOption0").click();
+Game.OpenSesame();
+Game.Notify('Using Bots arn\'t cheating... are they?','Cheated cookies tastes just fine.',[0,31]);

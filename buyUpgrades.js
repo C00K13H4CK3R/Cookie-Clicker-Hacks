2747b7fb2832455d89bfffd5fa00f445ec0cd72c
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-03 21:34:24 -0400

    buyUpgrades.js
    Automattically buys upgrades when you have enough cookies.

---
+++ buyUpgrades.js
@@ -0,0 +1,13 @@
+/* Buys upgrades when you have enough cookies.
+If you want to get all upgrades without using your cookies, simply use Game.SetAllUpgrades(1); instead. */
+
+var upgradeNum = 0;
+function buyUpgrades() {
+   var upgradeLength = Game.UpgradesById.length;
+   while (upgradeNum < upgradeLength) {
+       Game.UpgradesById[upgradeNum].toggle();
+       upgradeNum ++;
+   }
+}
+
+setInterval(buyUpgrades, 100);

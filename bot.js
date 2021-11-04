af17d78c25617881103f43c2c800901d3c1370e5
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-04 16:58:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,4 +7,14 @@ if (window.location.href == "https://orteil.dashnet.org/cookieclicker/") {
   document.getElementById("promptOption0").click();
   Game.OpenSesame();
   Game.Notify('Using Bots isn\'t cheating... is it?','Cheated cookies tastes just fine.',[12,0]);
+  
+  /* For clicking cookies */
+  var autoClicker = setInterval(Game.ClickCookie, 200);
+  
+  /* For clicking gold cookies */
+  setInterval(function() {
+    Game.shimmers.forEach(function(shimmer) {
+        if (shimmer.type == "golden") { shimmer.pop() }
+    })
+}, 500);
 }

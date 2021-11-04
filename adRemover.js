fd20305a21e3455ae07d74581fa2f85fabacacbc
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-03 21:20:58 -0400

    adRemover.js
    Removes Pop-Up ads in Cookie Clicker.

---
+++ adRemover.js
@@ -0,0 +1,6 @@
+/* Ad Remover By: Benjamin Kim */
+/* Open Console (CTRL + SHIFT + J) in Cookie Clicker and Copy/Paste the following. */
+
+document.getElementById("smallSupport").remove();
+document.getElementById("support").remove();
+document.getElementById("topBar").remove();

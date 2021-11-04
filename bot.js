41ddd794decc7b0bbe9165457726d0e4a212b1dc
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-04 09:16:38 -0400

    bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,5 @@
 // In developement
-console.log("Copyright (c) 2021 eunhyobenjaminkim \n" +
-"Permission is hearby granted, free of charge. You may copy, modify, merge, publish, distribute, sublicense, and/or sell \n" +
-"copies of the Programs as long as you are subject to the following conditions: \n" +
-"You are to give credit and/or provide the link to the original Program. Note, that I will not be responsible for any unwanted changes to your game. \n" +
-"Feel free to modify the program to your desire, following the conditions above. \n" +
-"Thank you, and Enjoy!")
+console.log("Copyright (c) 2021 eunhyobenjaminkim");
 document.getElementById("bakeryName").click();
 document.getElementById("bakeryNameInput").value = "Bot";
 document.getElementById("promptOption0").click();

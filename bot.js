e725fd881df5768f8270d33c322538858c72893e
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-04 09:23:19 -0400

    bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-// In developement
+/* In developement */
 console.log("Copyright (c) 2021 eunhyobenjaminkim");
 document.getElementById("bakeryName").click();
 document.getElementById("bakeryNameInput").value = "Bot";
 document.getElementById("promptOption0").click();
 Game.OpenSesame();
-Game.Notify('Using Bots arn\'t cheating... are they?','Cheated cookies tastes just fine.',[0,31]);
+Game.Notify('Using Bots isn\'t cheating... is it?','Cheated cookies tastes just fine.',[0,31]);

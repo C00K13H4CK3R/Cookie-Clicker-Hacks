2bb39248cae543084cb8a2a5e7884a99004ad960
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-04 10:52:34 -0400

    Update buyUpgrades.js

--- buyUpgrades.js
+++ buyUpgrades.js
@@ -2,6 +2,7 @@
 If you want to get all upgrades without using your cookies, simply use Game.SetAllUpgrades(1); instead. */
 
 // Does not work (I will try to fix this)
+// Game.UpgradesById[0].click(event);
 
 var upgradeNum = 0;
 function buyUpgrades() {

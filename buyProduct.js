d436a0d7a786579d766773863281bd68ca7a9f8c
eunhyobenjaminkim <72532132+eunhyobenjaminkim@users.noreply.github.com>
2021-11-05 19:02:22 -0400

    buyProduct.js
    /* Automatically buys a product (item) repetitively, once you have enough cookies. Note: you can only choose one product */

--- buyProduct.js
+++ buyProduct.js
@@ -1,4 +1,4 @@
-/* Automatically buys products (items), once you have enough cookies. Note: you can only choose one product */
+/* Automatically buys a product (item) repetitively, once you have enough cookies. Note: you can only choose one product */
 
 var productIdNum = prompt("Enter product number. (For Example, \n Cursor = 0, \n Grandma = 1, \n Farm = 2, \n (e.t.c.))");
 function buy() {

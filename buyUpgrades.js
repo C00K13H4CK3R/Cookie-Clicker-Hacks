caca3cff987a3e0408751e89de31232c0ccd5b49
Benjamin Kim <72532132+C00K13H4CK3R@users.noreply.github.com>
2021-11-11 14:09:25 -0500

    buyUpgrades.js

--- buyUpgrades.js
+++ buyUpgrades.js
@@ -1,16 +1,14 @@
 /* Buys upgrades when you have enough cookies.
 If you want to get all upgrades without using your cookies, simply use Game.SetAllUpgrades(1); instead. */
 
-// Does not work (I will try to fix this)
-// Game.UpgradesById[0].click(event);
-
 var upgradeNum = 0;
 function buyUpgrades() {
    var upgradeLength = Game.UpgradesById.length;
    while (upgradeNum < upgradeLength) {
-       Game.UpgradesById[upgradeNum].toggle();
+       Game.UpgradesById[upgradeNum].click(event);
        upgradeNum ++;
    }
 }
 
+PlaySound('snd/tick.mp3');Game.ClosePrompt();
 setInterval(buyUpgrades, 100);
